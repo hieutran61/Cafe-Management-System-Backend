0280c10206322d3ff55d8e7320f2c518e96e6e0b
Hieu Tran <hieutran6102@gmail.com>
2023-04-13 02:09:33 +0700

    add model Product

---
+++ src/main/java/com/hieutran/cafe/model/Product.java
@@ -0,0 +1,33 @@
+package com.hieutran.cafe.model;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+public class Product implements Serializable {
+
+    public static final Long serialVersionUid = 123456L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    private String description;
+
+    private Integer price;
+
+    private String status;
+}
